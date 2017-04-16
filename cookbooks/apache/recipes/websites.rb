a1c43be5fc5f0d80ff21fa2cdfb83c5ff019a705
sateesh3550 <sateesh060892@gmail.com>
2017-04-16 21:43:07 +0000

    environments, role and all

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,6 +1,11 @@
 file 'default.www' do
 	path '/var/www/html/index.html'
-	content 'hello world'
+	content 'hello world v1.0'
 end
 
+#webnodes = search('node', 'role:web')
+
+#webnodes.each do |node|
+#	puts node
+#end
 
